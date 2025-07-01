d946e7f73fc042dd9727eca713838c1f1fd24904
Abdenbi1 <abdenbi.ennaissi@uit.ac.ma>
2025-07-01 16:15:32 +0200

    Add files via upload

---
+++ data/OTILK_1.js
@@ -0,0 +1,15 @@
+var json_OTILK_1 = {
+"type": "FeatureCollection",
+"name": "OTILK_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "اسم_ا": "Relax Hotel Kenitra", "الوصف": "فندق 3 نجوم مع مطعم ومسبح وخدمة Wi-Fi مجانية", "الإحد": 34.254, "الإح_1": -6.587 }, "geometry": { "type": "Point", "coordinates": [ -6.587, 34.254 ] } },
+{ "type": "Feature", "properties": { "اسم_ا": "Urban Hotel & Spa", "الوصف": "فندق 4 نجوم مع سبا ومسبح وخدمات ممتازة", "الإحد": 34.251, "الإح_1": -6.582 }, "geometry": { "type": "Point", "coordinates": [ -6.582, 34.251 ] } },
+{ "type": "Feature", "properties": { "اسم_ا": "Waves Aqua Resort", "الوصف": "منتجع عائلي به مسبح وحديقة", "الإحد": 34.249, "الإح_1": -6.588 }, "geometry": { "type": "Point", "coordinates": [ -6.588, 34.249 ] } },
+{ "type": "Feature", "properties": { "اسم_ا": "City Hotel by Waves", "الوصف": "فندق أنيق ضمن مجموعة Waves مع مطعم وبار", "الإحد": 34.252, "الإح_1": -6.584 }, "geometry": { "type": "Point", "coordinates": [ -6.584, 34.252 ] } },
+{ "type": "Feature", "properties": { "اسم_ا": "Razane Hôtel", "الوصف": "فندق متوسط السعر للأزواج والعائلات", "الإحد": 34.2535, "الإح_1": -6.5865 }, "geometry": { "type": "Point", "coordinates": [ -6.5865, 34.2535 ] } },
+{ "type": "Feature", "properties": { "اسم_ا": "Hotel Vitality Terminus", "الوصف": "فندق 4 نجوم وسط المدينة بموقف مجاني", "الإحد": 34.255, "الإح_1": -6.583 }, "geometry": { "type": "Point", "coordinates": [ -6.583, 34.255 ] } },
+{ "type": "Feature", "properties": { "اسم_ا": "Hotel Mamora Kenitra", "الوصف": "فندق بتقييم جيد ومرافق سبا", "الإحد": 34.256, "الإح_1": -6.58 }, "geometry": { "type": "Point", "coordinates": [ -6.58, 34.256 ] } },
+{ "type": "Feature", "properties": { "اسم_ا": "Hotel D’Europe Centre Ville", "الوصف": "فندق اقتصادي في وسط المدينة", "الإحد": 34.253, "الإح_1": -6.585 }, "geometry": { "type": "Point", "coordinates": [ -6.585, 34.253 ] } }
+]
+}
